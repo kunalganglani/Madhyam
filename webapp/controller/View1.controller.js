7691d57cc9214762db81fe2ac9a21516cbd15e9b
kunalganglani <kunalganglani@gmail.com>
2016-11-30 10:41:13 +0000

    generic tiles for InOTify

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,23 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
-	"use strict";
+sap.ui.define(['jquery.sap.global', 'sap/ui/core/mvc/Controller', 'sap/m/MessageToast'],
+	function(jQuery, Controller, MessageToast) {
+		"use strict";
 
-	return Controller.extend("madhyam.controller.View1", {
+		return Controller.extend("madhyam.controller.View1", {
+			onInit: function() {
+				var oModel = new sap.ui.model.json.JSONModel(jQuery.sap.getResourcePath("sap/m/sample/GenericTileLineMode/tiles.json"));
+				this.getView().setModel(oModel);
+			},
+
+			statusPress: function(evt) {
+				var oTile = evt.getSource();
+				MessageToast.show(oTile.getHeader() + " was pressed.");
+			},
+
+			infoPress: function(evt) {
+				var oTile = evt.getSource();
+				MessageToast.show(oTile.getHeader() + " was pressed.");
+			}
+
+		});
 
 	});
-
-});
