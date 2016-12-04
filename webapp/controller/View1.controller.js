d376b92a92f5d8d7fe97c451a6e696ad57a54770
kunalganglani <kunalganglani@gmail.com>
2016-12-04 14:14:03 +0000

    Last checked updated on refresh

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -11,17 +11,17 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/mvc/Controller', 'sap/m/Message
 				var oModel = new sap.ui.model.json.JSONModel(sUrl);
 				sap.ui.getCore().setModel(oModel);
 				var aData = [
-				{material_id : "A1", material_name : "B1", material_status : 0,material_desc: "xyz"},
-				{material_id : "A2", material_name : "B2", material_status : 0, material_desc: "xyz"},
-				{material_id : "A3", material_name : "B3", material_status : 1, material_desc: "xyz"},
-				{material_id : "A4", material_name : "B2", material_status : 1, material_desc: "xyz"},
-				{material_id : "A2", material_name : "B3", material_status : 2,  material_desc: "xyz"},
-				{material_id : "A3", material_name : "B1", material_status : 2,  material_desc: "xyz"},
-				{material_id : "A4", material_name : "B1", material_status : 0, material_desc: "xyz"},
-				{material_id : "A1", material_name : "B3", material_status : 0, material_desc: "xyz"},
-				{material_id : "A1", material_name : "B3", material_status : 0, material_desc: "xyz"},
-				{material_id : "A2", material_name : "B2", material_status : -1,material_desc: "xyz"},
-				{material_id : "A4", material_name : "B1", material_status : -1,material_desc: "xyz"}
+				{material_id : "1", material_name : "B1", material_status : 0,material_desc: "10/10 Found", sensor_type:"weight", sensor_model:"xt100", last_check:"2016/12/4"},
+				{material_id : "2", material_name : "B2", material_status : 0, material_desc: "10/10 Found", sensor_type:"weight", sensor_model:"xt100", last_check:"2016/12/4"},
+				{material_id : "3", material_name : "B3", material_status : 1, material_desc: "9/10 Missing Item", sensor_type:"weight", sensor_model:"xt100", last_check:"2016/12/4"},
+				{material_id : "4", material_name : "B2", material_status : 1, material_desc: "8/10 Missing Item", sensor_type:"weight", sensor_model:"xt100", last_check:"2016/12/4"},
+				{material_id : "5", material_name : "B4", material_status : 2,  material_desc: "Improper Environment", sensor_type:"temperature", sensor_model:"xt100", last_check:"2016/12/4"},
+				{material_id : "6", material_name : "B5", material_status : 2,  material_desc: "Improper Environment", sensor_type:"temperature", sensor_model:"xt100", last_check:"2016/12/4"},
+				{material_id : "7", material_name : "B6", material_status : 0, material_desc: "10/10 Found", sensor_type:"weight", sensor_model:"xt100", last_check:"2016/12/4"},
+				{material_id : "8", material_name : "B7", material_status : 0, material_desc: "10/10 Found", sensor_type:"weight", sensor_model:"xt100", last_check:"2016/12/4"},
+				{material_id : "9", material_name : "B8", material_status : 0, material_desc: "10/10 Found", sensor_type:"weight", sensor_model:"xt100", last_check:"2016/12/4"},
+				{material_id : "10", material_name : "B9", material_status : -1,material_desc: "", sensor_type:"weight", sensor_model:"xt100", last_check:"2016/12/4"},
+				{material_id : "11", material_name : "B10", material_status : -1,material_desc: "", sensor_type:"weight", sensor_model:"xt100", last_check:"2016/12/4"}
 			];
 				for (var i = 0; i < aData.length; i++) {
 					
@@ -46,6 +46,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/mvc/Controller', 'sap/m/Message
 						aData[i].iconType="";
 					aData[i].m_status_text="Unkonwn";
 					aData[i].m_status="None";
+					aData[i].material_desc="Connection failed";
 					}
 				}
 			
@@ -65,13 +66,34 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/mvc/Controller', 'sap/m/Message
 													  	icon:"{iconType}"
 													  }),
 													  new sap.m.Label({
+														  text : "{sensor_type}"
+													  }),
+													  new sap.m.Label({
+														  text : "{sensor_model}"
+													  }),
+													  new sap.m.Label({
+														  text : "{last_check}"
+													  }),
+													  new sap.m.Label({
 														  text : "{material_desc}"
 													  })
+													  
 												  ]
 			}));
 				this.getView().setModel(oModel);
 			},
 
+			onTableRefresh: function(evt){
+				// var oTable= this.getView().byId("idPrdList");
+				var dateNow= new Date();
+				var last_check_String=dateNow.toLocaleString();
+				var oTable=evt.getSource().getParent().getParent();
+				for(var i=0;i<oTable.getItems().length;i++){
+				oTable.getItems()[i].getCells()[5].setText(last_check_String);	
+				}
+				
+				
+			},
 			
 			statusPress: function(evt) {
 				var oTile = evt.getSource();
